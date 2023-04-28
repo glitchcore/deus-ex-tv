d8eede7182c74d4a3d60337fa97de0c82cd68541
coreglitch <mail@s3f.ru>
2023-04-28 04:24:53 +0400

    multilang output

--- deus.py
+++ deus.py
@@ -57,6 +57,17 @@ def generate_poetry_thread(ai, conn, conn_mutex, event):
             event.wait()
             event.clear()
 
+def word_getter(poem):
+    print("new getter")
+    for line in zip(*poem.values()):
+        print(line)
+        lang_words = [lang.split(" ") for lang in line]
+        l = max([len(x) for x in lang_words])
+
+        for i in range(l):
+            p = i / l
+            yield "\n".join([x[int(p * len(x) + 0.1)] for x in lang_words])
+
 def get_poetry_line(conn):
     while True:
         c = conn.cursor()
@@ -66,16 +77,36 @@ def get_poetry_line(conn):
         conn_mutex.release()
 
         if len(rows) > 0:
-            for line in rows[0][1].split("\n"):
-                for word in line.split(" "):
-                    yield word
+            words = word_getter(json.loads(rows[0][1]))
+            try:
+                while True:
+                    yield next(words)
+            except StopIteration:
+                print("stop")
+                pass
 
-            # conn_mutex.acquire()
-            # c.execute("UPDATE \"poetry\" SET sent = True WHERE id = (?)", (rows[0][0],))
-            # conn.commit()
-            # conn_mutex.release()
+            conn_mutex.acquire()
+            c.execute("UPDATE \"poetry\" SET sent = True WHERE id = (?)", (rows[0][0],))
+            conn.commit()
+            conn_mutex.release()
         else:
             # select random row
+            print("select random row")
+            conn_mutex.acquire()
+            try:
+                c.execute("SELECT id FROM \"poetry\"")
+                ids = list(c.fetchall())
+                poetry_id = random.choice(ids)[0]
+                c.execute("SELECT poem FROM \"poetry\" WHERE id = (?) LIMIT 1", (poetry_id,))
+                words = word_getter(json.loads(list(c.fetchall())[0][0]))
+            finally:
+                conn_mutex.release()
+
+        try:
+            while True:
+                yield next(words)
+        except StopIteration:
+            print("stop")
             pass
 
 if __name__ == "__main__":
@@ -91,19 +122,22 @@ if __name__ == "__main__":
 
     class SimpleHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         def do_GET(self):
-            self.send_response(200)
-            self.send_header('Content-type', 'text/plain')
-            self.end_headers()
-            
-            # Write the response body
-            line = next(poetry_getter)
-            message = [
-                line,
-                line,
-                line
-            ]
-            message = "\n".join(message)
-            self.wfile.write(message.encode())
+            if self.path == '/':
+                self.send_response(200)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                
+                # Write the response body
+                message = next(poetry_getter)
+                self.wfile.write(message.encode())
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/plain')
+                self.end_headers()
+                
+                # Write the response body
+                message = ""
+                self.wfile.write(message.encode())
         
         def do_POST(self):
             content_length = int(self.headers.get('Content-Length', 0))
@@ -135,6 +169,9 @@ if __name__ == "__main__":
     with socketserver.TCPServer(("0.0.0.0", PORT), handler) as httpd:
         print("Server listening on port", PORT)
         # Start the server and keep it running until interrupted
-        httpd.serve_forever()
+        try:
+            httpd.serve_forever()
+        finally:
+            httpd.server_close()
 
 
